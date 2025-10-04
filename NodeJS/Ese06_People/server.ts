0236f114930af7f4fa4b0bc04b2230fb0ba7cf38
Tommaso <t.paparesta@gmail.com>
2025-10-04 15:15:07 +0200

    COntinuato ese06

--- NodeJS/Ese06_People/server.ts
+++ NodeJS/Ese06_People/server.ts
@@ -42,6 +42,7 @@ app.use("/", function (req: express.Request, res: express.Response, next: expres
 // 2. Gestione risorse statiche
 app.use("/", express.static("./static"));
 
+
 // 3. Lettura dei parametri POST
 // i parametri post sono restituiti come json all'interno di req.body
 // i parametri get sono restituiti come json all'interno di req.query
@@ -75,12 +76,58 @@ app.get("/api/countries", function (req: express.Request, res: express.Response)
     res.send(Array.from(new Set(countries.sort())));
     
 })
+app.get("/api/getPeopleByCountry", function (req: express.Request, res: express.Response) {
+    const country = req.query.country as string;
+    let peopleFilter: object[] = [];
 
+    for(const person of people.results){
+        if(person.location.country==country){
+            const object:object={
+                name:{
+                     title: person.name.title,
+                    first: person.name.first,
+                    last: person.name.last
+                },
+                city:person.location.city,
+                state:person.location.state,
+                cell: person.cell,
+            }
+            peopleFilter.push(object)
+         
+        }
+    }
+    res.send(peopleFilter);
+});
+
+app.get("/api/getPerson",function (req: express.Request, res: express.Response){
+    const personStr = req.query.person as string;
+    const person = JSON.parse(decodeURIComponent(personStr));
+    if(!person){
+        res.status(404);
+        res.send("Errore persona");
+        return;
+    }
+    const findPerson = people.results.find(p =>
+        p.name.title === person.name.title &&
+        p.name.first === person.name.first &&
+        p.name.last === person.name.last
+    );
+
+
+    if(findPerson){
+        res.send(findPerson);
+    }
+    else{
+        res.status(404);
+        res.send("Persona non trovata nella lista");
+    }
+})
 // F) default root 
 app.use("/", function (req: express.Request, res: express.Response) {
     res.status(404);
     if(!req.originalUrl.startsWith("/api/")) {
         res.send(paginaErr);// send serializza in automatico
+        
     }
     else {
         res.send("Risorsa non trovata");
--- NodeJS/Ese06_People/static/index.js
+++ NodeJS/Ese06_People/static/index.js
@@ -1,20 +1,24 @@
 "use strict"
 
-const { get } = require("http");
+
 
 let peopleList;  // vettore enumerativo delle Persone attualmente visualizzate
                  // comodo per gestire i pulsanti di navigazione
-let currentPos;   
+
 const lstCountries = document.getElementById("lstCountries")
 // i seguenti puntatori sono tutti definiti tramite ID
 // let lstCountries 
 // let tabStudenti  
 // let divDettagli 
-
+const btnFirst=document.querySelectorAll("#divDettagli a")[0]
+const btnPrev=document.querySelectorAll("#divDettagli a")[1]
+const btnNext=document.querySelectorAll("#divDettagli a")[2]
+const btnLast=document.querySelectorAll("#divDettagli a")[3]
+let cardIndex=0
 btnAdd.addEventListener("click", function(){
 	window.location.href = "./inserisci.html"
 })
-
+const allPeople=[]
 divDettagli.style.display="none"
 getCountries()
 
@@ -29,7 +33,7 @@ async function getCountries(){
             a.href = "#"
             a.textContent = country
             a.addEventListener("click", function(){
-                getPeopleByCountry()
+                getPeopleByCountry(a.textContent)
             })
             lstCountries.appendChild(a)
         }
@@ -39,13 +43,129 @@ async function getCountries(){
     }
 }
 
-async function getPeopleByCountry(){
-    let response= await inviaRichiesta("GET", "/api/getPeopleByCountry?country=" + this.textContent)
-    if(response.status=200){
-        console.log(response.data)
-
-    }
-    else{
-        alert(response.status + ": " + response.err)
+async function getPeopleByCountry(country){
+    let response = await inviaRichiesta("GET", "/api/getPeopleByCountry?country=" + country);
+    if (response.status === 200) {
+        peopleList = response.data;
+        createTable(response.data);
+    } else {
+        alert(response.status + ": " + response.err);
     }
 }
+
+function createTable(people){
+    let tbody=document.querySelector("#tabStudenti");
+    tbody.innerHTML="";
+    let count=0
+   
+    Array.from(people).forEach((person, index) => {
+        count++
+        let tr=document.createElement("tr")
+        let td=document.createElement("td")
+
+        td.textContent=person.name.title + " " + person.name.first + " " + person.name.last
+        tr.appendChild(td)
+
+        td=document.createElement("td")
+        td.textContent=person.city
+        tr.appendChild(td)
+
+        td=document.createElement("td")
+        td.textContent=person.state
+        tr.appendChild(td)
+
+        td=document.createElement("td")
+        td.textContent=person.cell
+        tr.appendChild(td)
+
+        let btnDettagli=document.createElement("button")
+        btnDettagli.classList.add("btn","btn-secondary")
+        btnDettagli.textContent="Dettagli"
+        btnDettagli.addEventListener("click", () => {
+            cardIndex = index;
+            showDetails(cardIndex);
+        });
+
+        td=document.createElement("td")
+        td.appendChild(btnDettagli)
+        tr.appendChild(td)
+
+        let btnElimina=document.createElement("button")
+        btnElimina.classList.add("btn","btn-danger")
+        btnElimina.textContent="Elimina"
+        btnElimina.addEventListener("click", function(){
+            deletePerson(person)
+        })
+        td=document.createElement("td")
+        td.appendChild(btnElimina)
+        tr.appendChild(td)
+        tbody.appendChild(tr)
+    })
+}
+
+async function showDetails(index){
+    if (!peopleList || peopleList.length === 0) return;
+
+
+
+    cardIndex = index;
+    const person=peopleList[cardIndex]
+    let response = await inviaRichiesta("GET", 
+        "/api/getPerson?person=" + encodeURIComponent(JSON.stringify(person)));
+    if (response.status === 200) {
+        const person=response.data
+        divDettagli.style.display="block"
+
+
+        let img=document.querySelector("#divDettagli img")
+        img.src=person.picture.large
+
+        let h5=document.querySelector("#divDettagli h5")
+        h5.textContent=person.name.first + " " + person.name.last
+
+        let pCardText=document.querySelector("#divDettagli .card-text")
+        pCardText.innerHTML=""
+
+        let gender=document.createElement("p")
+        gender.innerHTML=`<b>Gender:</b> ${person.gender}`
+
+        let address=document.createElement("p")
+        let stringAddress=`<b>Address:</b> ${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}, ${person.location.country}`
+        address.innerHTML=stringAddress
+
+        let email=document.createElement("p")
+        email.innerHTML=`<b>Email:</b> ${person.email}`
+
+        let date=document.createElement("p")
+        date.innerHTML=`<b>Date of Birth:</b> ${person.dob.date.split("T")[0]}`
+        pCardText.appendChild(email)
+
+        pCardText.appendChild(gender)
+        pCardText.appendChild(address)
+        pCardText.appendChild(date)
+    } else {
+        alert(response.status + ": " + response.err);
+    }
+   
+}
+btnFirst.addEventListener("click", function(){
+    
+    showDetails(0);
+});
+btnPrev.addEventListener("click", function(){
+    if (cardIndex === 0){
+        alert("Non puoi tornare indietro")
+        return
+    };
+    showDetails(cardIndex - 1);
+});
+btnNext.addEventListener("click", function(){
+    if (cardIndex === peopleList.length - 1){
+        alert("Non puoi andare avanti")
+        return
+    };
+    showDetails(cardIndex + 1);
+});
+btnLast.addEventListener("click", function(){
+    showDetails(peopleList.length - 1);
+});
